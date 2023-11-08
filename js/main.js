8a9224455945af3923c9146f8991fd2302391186
PinoGabriel <pinogabriel01@gmail.com>
2023-11-08 17:31:50 +0100

    feat: added 16 bomb

--- js/main.js
+++ js/main.js
@@ -5,35 +5,39 @@ let containerMain = document.getElementById("container_main")
 
 let level = document.getElementById("level");
 
-const cellaFortunata_easy = getRndInteger(1, 100)
-const cellaFortunata_med = getRndInteger(1, 81)
-const cellaFortunata_hard = getRndInteger(1, 49)
-console.log("cella fortunata hard:", cellaFortunata_hard);
-console.log("cella fortunata med:",cellaFortunata_med);
-console.log("cella fortunata easy:",cellaFortunata_easy);
-
+let cellaFortunata;
+ 
 const gameOver = document.getElementById("gameOver");
 
+let bombe = [];
 
+
+/*********** INIZIO GIOCO *********/
 
 play.addEventListener("click", function () {
     containerMain.innerHTML = ""
 
     if (level.value == "easy") {
         console.log("selezionato easy");
-        generaGriglia(100, "square_easy", cellaFortunata_easy);
+        bombeFino(100)
+        console.log("le bombe sono:", bombe);
+        generaGriglia(100, "square_easy", bombe);
     } else if (level.value == "med") {
         console.log("selezionato med");
-        generaGriglia(81, "square_medium", cellaFortunata_med)
+        bombeFino(81)
+        console.log("le bombe sono:", bombe);
+        generaGriglia(81, "square_medium", bombe)
     } else {
-        generaGriglia(49, "square_hard", cellaFortunata_hard)
+        bombeFino(49)
+        console.log("le bombe sono:", bombe);
+        generaGriglia(49, "square_hard", bombe)
     }
 
 })
 
+/*********** FUNZIONE GENERALE *********/
 
-
-function generaGriglia(numeroCelle, className, cellaFortunata) {
+function generaGriglia(numeroCelle, className, bombe) {
     for (let i = 1; i <= numeroCelle; i++) {
         const div = document.createElement("div");
         div.classList.add(className);
@@ -44,7 +48,7 @@ function generaGriglia(numeroCelle, className, cellaFortunata) {
             console.log(i);
             div.classList.toggle("square_bg");
 
-            if (i == cellaFortunata) {
+            if (bombe.includes(i)) {
                 div.classList.remove("square_bg")
                 div.classList.add("bg_red")
                 gameOver.classList.remove("d-none")
@@ -55,6 +59,22 @@ function generaGriglia(numeroCelle, className, cellaFortunata) {
     }
 }
 
+/*********** FUNZIONE GENERA NUMERI CASUALI *********/
+
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
+
+
+
+/*********** FUNZIONE CILCO BOMBE *********/
+function bombeFino(numeroCasualeBombe) {
+    for (let i = 1; i < 16; i++) {
+        let bombeCasuali = getRndInteger(1, numeroCasualeBombe);
+    
+        bombe.push(bombeCasuali)
+        
+    }
+    
+    }
+  
